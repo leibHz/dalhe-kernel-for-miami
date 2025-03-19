8e590ea149f228274dc8b2080873d55eb0a218a9
Michael Bestas <mkbestas@lineageos.org>
2025-03-19 09:16:15 +0200

    mmc: sdhci-msm: Move MMC_CAP2_MAX_DISCARD_SIZE to prevent merge conflict
    Change-Id: I80acdb4f52811ebd371317acfc4afa41e4960656

--- drivers/mmc/host/sdhci-msm.c
+++ drivers/mmc/host/sdhci-msm.c
@@ -4555,12 +4555,13 @@ static int sdhci_msm_probe(struct platform_device *pdev)
 	}
 
 	sdhci_msm_set_caps(msm_host);
+	msm_host->mmc->caps2 |= MMC_CAP2_MAX_DISCARD_SIZE;
 
 #if defined(CONFIG_SDC_QTI)
 	host->timeout_clk_div = 4;
 	msm_host->mmc->caps2 |= MMC_CAP2_CLK_SCALE;
 #endif
-	msm_host->mmc->caps2 |= MMC_CAP2_MAX_DISCARD_SIZE;
+
 	sdhci_msm_setup_pm(pdev, msm_host);
 
 	host->mmc_host_ops.execute_tuning = sdhci_msm_execute_tuning;
